dec5457627d6cde0c50547f08cad7d0a2c30744e
Martin Prusa <martin@martinprusa.com>
2022-01-14 22:09:11 +0100

    pasteboard

---
+++ Sources/MKit/MFoundation/Workers/PasteboardWorker.swift
@@ -0,0 +1,25 @@
+//
+//  PasteboardWorker.swift
+//  
+//
+//  Created by Martin Prusa on 14.01.2022.
+//
+
+import Foundation
+import MobileCoreServices
+
+public struct PasteboardWorker {
+    private let pasteboard: UIPasteboard
+
+    public init(pasteboard: UIPasteboard = .general,) {
+        self.pasteboard = pasteboard
+    }
+
+    public func set(text: String) {
+        pasteboard.setItems([[kUTTypeUTF8PlainText as String: text]])
+    }
+
+    public func text() -> String? {
+        pasteboard.string
+    }
+}
