6885282d0fb64c48e137624a027ce55772b53543
Martin Prusa <martin@martinprusa.com>
2025-02-09 19:47:05 +0100

    ssl certificate validation

--- Package.swift
+++ Package.swift
@@ -20,16 +20,22 @@ let package = Package(
         )
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-certificates.git", .upToNextMajor(from: "1.7.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MKit",
-            dependencies: []),
-        .target(name: "MSecurity", dependencies: [], path: "Sources/MSecurity"),
+            dependencies: [
+                .product(name: "X509", package: "swift-certificates")
+            ]
+        ),
+        .target(
+            name: "MSecurity",
+            dependencies: [],
+            path: "Sources/MSecurity"
+        ),
         .testTarget(
             name: "MKitTests",
             dependencies: ["MKit"]),
